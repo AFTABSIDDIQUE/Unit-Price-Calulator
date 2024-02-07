d85eda7d37d1337be946adf5d1e129f9166338ce
aftabsiddique <mohammedsiddiquezee@gmail.com>
2024-02-07 21:13:00 +0530

    Responsive commit

--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -7,7 +7,7 @@ function InputForm({ unitsConsumed, setUnitsConsumed, calculatePrice }) {
         <span class="input-group-text">Units Consumed</span>
         <input className="col-sm-12" type="text" value={unitsConsumed} onChange={(e) => setUnitsConsumed(e.target.value)} class="form-control" placeholder="Enter Units" aria-label="Recipient's username" aria-describedby="button-addon2" />
         <div class="col-12">
-          <button class="btn btn-primary mt-3" onClick={calculatePrice} type="submit">Calculate</button>
+          <button class="btn btn-outline-primary mt-3" onClick={calculatePrice} type="submit">Calculate</button>
         </div>
       </div>
     </div>
